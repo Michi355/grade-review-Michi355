a1ef1216fe8d23df89b0f6cb1462d658be0ade17
holden <holden.pipp@gmail.com>
2023-05-11 18:36:02 -0700

    bad coding practice 2

--- grade.sh
+++ grade.sh
@@ -5,8 +5,8 @@ rm -rf grading-area
 
 # step 1 - clone relevant to specific directory
 mkdir grading-area
-
 git clone $1 student-submission
+
 echo 'Finished cloning'
 
 # step 2 - check for relevant files
@@ -28,9 +28,13 @@ fi
 cp student-submission/*/*.java grading-area
 
 # step 4
-if [[ javac grading-area/*.java -eq 1 ]]
+javac grading-area *.java > javac_output.txt
+if ! [[ $? -eq 0 ]]
 then
-
+    echo 'Yippee'
+else
+    echo javac_output.txt
+    exit
 fi
 
 # Draw a picture/take notes on the directory structure that's set up after
